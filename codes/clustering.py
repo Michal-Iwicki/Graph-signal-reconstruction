b22cbd21e7a757c4a5e1ac35c9f64491d00873bb
Michał <michal.iwicki00@gmail.com>
2026-04-06 18:51:44 +0200

    Refactored

--- codes/clustering.py
+++ codes/clustering.py
@@ -1,43 +1,26 @@
 import numpy as np
 from scipy.special import logsumexp
-from sklearn.metrics import accuracy_score
-from scipy.optimize import linear_sum_assignment
-from codes.generation import generate_mixed_signals
-from codes.reconstruction import reconstruct_smooth
 from sklearn.cluster import KMeans
+from scipy.optimize import linear_sum_assignment
 
-
-# =========================================
-# GMM DIAGONAL EM
-# =========================================
 class GMM_Diag:
-    def __init__(self, K, max_iter=100, tol=1e-4, reg=1e-6, min_cluster_frac=0.05):
+    def __init__(self, K, max_iter=100, tol=1e-4, reg=1e-6):
         self.K = K
         self.max_iter = max_iter
         self.tol = tol
         self.reg = reg
-        self.min_cluster_frac = min_cluster_frac
 
     def _log_gaussian(self, X, mu, var):
-        return -0.5 * (
-            np.sum(np.log(2 * np.pi * var)) +
-            np.sum((X - mu) ** 2 / var, axis=1)
-        )
+        return -0.5 * (np.sum(np.log(2 * np.pi * var)) + np.sum((X - mu) ** 2 / var, axis=1))
 
     def fit(self, X):
-        """
-        X: (M, D)
-        """
         M, D = X.shape
-
-
         kmeans = KMeans(n_clusters=self.K, n_init=5)
         labels = kmeans.fit_predict(X)
 
         self.mu = kmeans.cluster_centers_
-
-        # opcjonalnie lepsza inicjalizacja wariancji
         self.var = np.zeros((self.K, D))
+        
         for k in range(self.K):
             cluster_points = X[labels == k]
             if len(cluster_points) > 0:
@@ -52,22 +35,16 @@ class GMM_Diag:
         prev_ll = -np.inf
 
         for _ in range(self.max_iter):
-
-            # ===== E-step =====
             log_resp = np.zeros((M, self.K))
-
             for k in range(self.K):
                 log_resp[:, k] = np.log(self.pi[k]) + self._log_gaussian(X, self.mu[k], self.var[k])
 
             log_norm = logsumexp(log_resp, axis=1, keepdims=True)
             log_resp -= log_norm
             resp = np.exp(log_resp)
-
             ll = np.sum(log_norm)
 
-            # ===== M-step =====
             Nk = resp.sum(axis=0) + 1e-12
-
             self.pi = Nk / M
             self.mu = (resp.T @ X) / Nk[:, None]
 
@@ -77,7 +54,6 @@ class GMM_Diag:
             
             self.var += self.reg
 
-            # convergence
             if np.abs(ll - prev_ll) < self.tol:
                 break
             prev_ll = ll
@@ -85,59 +61,21 @@ class GMM_Diag:
     def predict(self, X):
         M = X.shape[0]
         log_resp = np.zeros((M, self.K))
-
         for k in range(self.K):
             log_resp[:, k] = np.log(self.pi[k]) + self._log_gaussian(X, self.mu[k], self.var[k])
-
         return np.argmax(log_resp, axis=1)
-    
 
-def clustering_accuracy(y_true, y_pred, K):
-    cost = np.zeros((K, K))
+class ClusteringEvaluator:
+    @staticmethod
+    def evaluate_accuracy(y_true, y_pred, K):
+        cost = np.zeros((K, K))
+        for i in range(K):
+            for j in range(K):
+                cost[i, j] = np.sum((y_true == i) & (y_pred == j))
+        row_ind, col_ind = linear_sum_assignment(-cost)
+        return cost[row_ind, col_ind].sum() / len(y_true)
 
-    for i in range(K):
-        for j in range(K):
-            cost[i, j] = np.sum((y_true == i) & (y_pred == j))
-
-    row_ind, col_ind = linear_sum_assignment(-cost)
-    return cost[row_ind, col_ind].sum() / len(y_true)
-
-def graph_fourier_features(graph, X):
-    U = graph.eigenvectors
-    return (U.T @ X).T  # (M, N)
-
-def gmm_mixed_signal_experiment(
-    graph,
-    psd_s,
-    probs,
-    M=500,
-    p = 1,
-    n_runs=10,
-    seed=42
-):
-    np.random.seed(seed)
-    accs = []
-    K =len(psd_s)
-    for _ in range(n_runs):
-
-        # ===== GENERACJA =====
-        X, Y, labels = generate_mixed_signals(graph, M, p = p, psd_s = psd_s, probs=probs)
-        if p < 1:
-            Y = reconstruct_smooth(graph=graph, y=Y, beta=0.0001)
-
-        X_feat = graph_fourier_features(graph, Y)
-
-        # ===== GMM =====
-        gmm = GMM_Diag(K)
-        gmm.fit(X_feat)
-        pred = gmm.predict(X_feat)
-        #print(np.bincount(pred))
-        # ===== EVAL =====
-        acc = clustering_accuracy(labels, pred, K)
-        accs.append(acc)
-
-    return {
-        "mean_acc": np.mean(accs),
-        "std_acc": np.std(accs),
-        "all_accs": accs
-    }
+    @staticmethod
+    def graph_fourier_features(graph, X):
+        U = graph.eigenvectors
+        return (U.T @ X).T
